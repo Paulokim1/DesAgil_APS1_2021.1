ba2f223e5b8c2a5e682ffa2cb87bdf276879391d
Paulokim1 <paulo.wook21@gmail.com>
2021-03-19 12:15:47 -0300

    produto, pedido e carrinho feitos

--- VaiMalte/src/br/edu/insper/desagil/vaimalte/Carrinho.java
+++ VaiMalte/src/br/edu/insper/desagil/vaimalte/Carrinho.java
@@ -1,5 +1,28 @@
 package br.edu.insper.desagil.vaimalte;
 
-public class Carrinho {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Carrinho {
+	private List<Pedido> lista_pedidos;
+	
+	public Carrinho() {
+		this.lista_pedidos = new ArrayList<>();
+	}
+
+	public List<Pedido> getLista_pedidos() {
+		return this.lista_pedidos;
+	}
+	
+	public List<Pedido> incrementa_lista(Produto produto){
+		for (Pedido pedido : this.lista_pedidos) {
+			if (pedido.getProduto() == produto) {
+				pedido.incrementa();
+				return this.lista_pedidos;
+			}
+		}
+		Pedido pedido_novo = new Pedido(produto);
+		this.lista_pedidos.add(pedido_novo);
+		return this.lista_pedidos;	
+	}
 }
--- VaiMalte/src/br/edu/insper/desagil/vaimalte/Pedido.java
+++ VaiMalte/src/br/edu/insper/desagil/vaimalte/Pedido.java
@@ -1,5 +1,31 @@
 package br.edu.insper.desagil.vaimalte;
 
 public class Pedido {
+	private Produto produto;
+	private int quantidade;
+	
+	public Pedido(Produto produto) {
+		this.produto = produto;
+		this.quantidade = 1;
+	}
 
+	public Produto getProduto() {
+		return this.produto;
+	}
+
+	public int getQuantidade() {
+		return this.quantidade;
+	}
+	
+	public int incrementa() {
+		return this.quantidade + 1;
+	}
+	
+	public int total() {
+		return this.produto.getPreco() * this.quantidade;
+	}
+
+
+	
+	
 }
